5a6f6be697b11ec10b9c17a87ea71efdb3195439
Umer Abdul Wahid <umerwahiid48@gmail.com>
2022-09-24 23:45:28 +0500

    done

---
+++ JAVASCRIPT/javaScript.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $("#FeedButton").click(function () {
+        $("#FeedContainer").fadeToggle();
+    });
+});
+
+$(document).ready(function () {
+    $("#AdsDivRemove").click(function () {
+        $("#AdsDiv").hide(1500,);
+    });
+});
+
+$(document).ready(function () {
+    $("#feedremove").click(function () {
+        $("#feedsticky").fadeOut(500,);
+    });
+});
+
+
+
+    
